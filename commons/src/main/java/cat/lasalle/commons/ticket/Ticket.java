254ac040d36f8066613e16de4dd59c77b80622ac
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-01-19 22:20:02 +0100

    feat(commons): add priority value

--- commons/src/main/java/cat/lasalle/commons/ticket/Ticket.java
+++ commons/src/main/java/cat/lasalle/commons/ticket/Ticket.java
@@ -14,19 +14,31 @@ public class Ticket {
     private final Set<User> assignees = new HashSet<>();
     private final Set<User> collaborators = new HashSet<>();
     private final List<Message> messages = new ArrayList<>();
-    @Setter private String description;
-    @Setter private Status status;
-    @Setter private Priority priority;
-    @Setter private Date lastUpdated;
-    @Setter private User requester;
+    @Setter
+    private String description;
+    @Setter
+    private Status status;
+    @Setter
+    private Priority priority;
+    @Setter
+    private Date lastUpdated;
+    @Setter
+    private User requester;
 
     public Ticket(long id, String subject) {
         this.id = id;
         this.subject = subject;
     }
 
+    @Getter
     public enum Priority {
-        LOW, MEDIUM, HIGH
+        LOW(1), MEDIUM(2), HIGH(3), CRITICAL(4);
+
+        private final int value;
+
+        Priority(int value) {
+            this.value = value;
+        }
     }
 
     public enum Status {
@@ -37,9 +49,12 @@ public class Ticket {
     public static class Message {
         private final long messageId;
         private final List<Message> replies = new ArrayList<>();
-        @Setter private User sender;
-        @Setter private Date sentDate;
-        @Setter private String body;
+        @Setter
+        private User sender;
+        @Setter
+        private Date sentDate;
+        @Setter
+        private String body;
 
         public Message(long messageId) {
             this.messageId = messageId;
