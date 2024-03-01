7c7b2d646119dcb87a043b077272627834ff6443
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-03-01 23:55:04 +0100

    feat(client): style and add ticket table

--- client/src/main/java/cat/lasalle/client/MainUserViewController.java
+++ client/src/main/java/cat/lasalle/client/MainUserViewController.java
@@ -1,5 +1,9 @@
 package cat.lasalle.client;
 
+import cat.lasalle.client.web.impl.TicketWebResource;
+import cat.lasalle.commons.ticket.TicketDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,13 +11,24 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainUserViewController {
     @FXML
     public Button newTicket;
+    @FXML
+    private TableView<TicketDTO> ticketTable;
+
+    public void initialize() {
+        TicketWebResource ticketWebResource = new TicketWebResource();
+        List<TicketDTO> tickets = ticketWebResource.readAll(); // Fetch the first 10 tickets
+        ObservableList<TicketDTO> observableList = FXCollections.observableArrayList(tickets);
+        ticketTable.setItems(observableList); // Bind the tickets to the TableView
+    }
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
